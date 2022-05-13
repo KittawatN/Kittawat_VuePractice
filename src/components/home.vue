ef3ed588a18c841f59ee5de31711e081f3ba20d7
KittawatN <105302372+KittawatN@users.noreply.github.com>
2022-05-13 17:30:01 +0700

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <card>2nd card</card>
+  </div>
+</template>
+
+<script>
+import card from "./card";
+
+export default {
+  comments: {
+    card,
+  },
+};
+</script>
